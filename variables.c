31213f0d86bfe226958d9c13ade8339a64473f9a
Mark Whitehouse <mark.whitehouse@sdhusd.net>
2021-01-06 22:34:04 +0000

    new file

---
+++ variables.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  int b = 3;
+  int c;
+
+  a = 2;
+  c = a + b;
+  printf("Sum of %d and %d is %d\n", a, b, c);
+
+ a = 5;
+  c = 2;
+  printf("a= %d, c= %d\n", a , c);
+
+}
