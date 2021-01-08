6e2d172546de6d1d3974f43090515931ad12489d
Mark Whitehouse <mark.whitehouse@sdhusd.net>
2021-01-08 06:31:00 +0000

    last commit

--- iteration.c
+++ iteration.c
@@ -12,7 +12,7 @@ void getTestInput(int argc, char* argv[], int* a)
 int main(int argc, char* argv[]) 
 {
   // the divisor variable
-  int div = 2;
+  int div = 5;
   
   // for testing only - do not change
   getTestInput(argc, argv, &div);
@@ -21,7 +21,7 @@ int main(int argc, char* argv[])
   // add code below to complete 2.4 for any value of div 
 	printf("looking for numbers divisible by %d\n", div);
 
-		for (int i = 0; i < 101; i++) {
+		for (int i = 0; i <= 100; i++) {
 			if (i % div == 0) {
 				printf("%d is divisible\n", i);
 		}
